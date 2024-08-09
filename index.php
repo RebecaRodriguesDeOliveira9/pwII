810180367d58bcd9264018dd7d54a8778f3d697d
rebeca <rebecarodriguesolver@gmail.com>
2024-08-09 20:48:50 -0300

    Aula de bootstrap

--- index.php
+++ index.php
@@ -4,9 +4,88 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Página inicial do site</title>
+    <link href="bootstrap.min.css" rel="stylesheet"/>
 </head>
 <body>
-    
+    <h1>Página inicial do site</h1>
+
+    <div class="container">
+
+    <div class="row"></div>
+        <div class="col-4"></div>
+        <div class="col-4">
+            <div class="alert alert-success" role="alert">
+                Cadastrado com sucesso
+</div>
+<div class="col-4"></div>
+</div>
+
+<div class="alert alert-success" role="alert">
+  A simple success alert—check it out!
+</div>
+
+<button type="button" class="btn btn-primary">Salvar cadastro</button>
+<button type="button" class="btn btn-secondary">Secondary</button>
+<button type="button" class="btn btn-success">Success</button>
+<button type="button" class="btn btn-danger">Danger</button>
+<button type="button" class="btn btn-warning">Warning</button>
+<button type="button" class="btn btn-info">Info</button>
+<button type="button" class="btn btn-light">Light</button>
+<button type="button" class="btn btn-dark">Dark</button>
+
+<button type="button" class="btn btn-link">Link</button>
+
+<a href="www.google.com.br" class="btn btn-primart"> Acesse o Google aqui</a>
+
+<table class="table table-striped table-bordered table-hover">
+  <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">First</th>
+      <th scope="col">Last</th>
+      <th scope="col">Handle</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>@mdo</td>
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td>Jacob</td>
+      <td>Thornton</td>
+      <td>@fat</td>
+    </tr>
+    <tr>
+      <th scope="row">3</th>
+      <td colspan="2">Larry the Bird</td>
+      <td>@twitter</td>
+    </tr>
+  </tbody>
+</table>
+
+<form>
+  <div class="mb-3">
+    <label for="exampleInputEmail1" class="form-label">Email address</label>
+    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+    <div id="emailHelp" class="form-text">Digite um EMAIL para acesar o sistema</div>
+  </div>
+  <div class="mb-3">
+    <label for="exampleInputPassword1" class="form-label">Password</label>
+    <input type="password" class="form-control" id="exampleInputPassword1">
+  </div>
+  <div class="mb-3 form-check">
+    <input type="checkbox" class="form-check-input" id="exampleCheck1">
+    <label class="form-check-label" for="exampleCheck1">Check me out</label>
+  </div>
+  <button type="submit" class="btn btn-success">Submit</button>
+</form>
+</div>
+
+    <script stc="bootstrap.bundle.min.js"></script>
 </body>
 </html>
