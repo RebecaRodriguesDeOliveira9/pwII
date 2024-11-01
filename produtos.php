13d18c971630663babd6dabcd4a5631a0a9ed0d0
rebeca <rebecarodriguesolver@gmail.com>
2024-10-31 22:30:53 -0300

    Aula-31_10_2024

--- produtos.php
+++ produtos.php
@@ -10,6 +10,7 @@ if( isset($_GET["pesquisa"]) )
        include "conexao.php";
        $sql = "Select Id, Descricao, Valor, Codigo_barras, Imagem from Produtos order by Id desc";
        $resultado = $conexao->query($sql);
+       
        $conexao->close();
     }
     else
@@ -18,11 +19,11 @@ if( isset($_GET["pesquisa"]) )
         include "conexao.php";
         $sql = "Select Id, Descricao, Valor, Codigo_barras, Imagem 
                 from Produtos  
-                where Descricao like '%$pesquisa%' || Codigo_Barras = $pesquisa
+                where Descricao like '%$pesquisa%' || Codigo_Barras = '$pesquisa'
                 order by Id desc";
         $resultado = $conexao->query($sql);
+        
         $conexao->close();
-    
     }
 }
 else
@@ -39,79 +40,88 @@ else
 
 ?>
 <br>
+<?php
+    if(isset($_GET["erro"]) && !empty($_GET["erro"]) )
+    {
+        echo "<div class='alert alert-danger'>";
+        echo $_GET["erro"];
+        echo "</div>";
+    }
+?>
+<br>
 <div class="row">
     <div class="col-12">
-      <div class="card">
-          <div class="card-header">
-        Lista de Produtos
-     </div>
-     <div class="card-body">
-         <div class="row">
-      <div class="col-2">
-  <a href="novo_produto.php" class="btn btn-success" >
-          Novo Produto
-      </a>
-      </div>
-        <div class="col-8">
-          <form action="produtos.php" method="get">
-          <div class="input-group mb-3">
-   <input type="text" 
- name="pesquisa" 
-   value="<?php echo $pesquisa; ?>"  
-   class="form-control" 
-         placeholder="Digite sua pesquisa aqui..." />
+        <div class="card">
+            <div class="card-header">
+                Lista de Produtos
+            </div>
+            <div class="card-body">
+                <div class="row">
+                    <div class="col-2">
+                        <a href="novo_produto.php" class="btn btn-success" >
+                            Novo Produto
+                        </a>
+                    </div>
+                    <div class="col-8">
+                        <form action="produtos.php" method="get">
+                            <div class="input-group mb-3">
+                                <input type="text" 
+                                        name="pesquisa" 
+                                        value="<?php echo $pesquisa; ?>"  
+                                        class="form-control" 
+                                        placeholder="Digite sua pesquisa aqui..." />
 
 
+                                <button class="btn btn-primary" type="submit">
+                                    Pesquisar
+                                </button>
+                            </div>
+                        </form>
+                    </div>
+                    <div class="col-2"></div>
+                </div>
 
- <button class="btn btn-primary" type="submit">
-             Pesquisar
-           </button>
-       </div>
-     </form>
-  </div>
-     <div class="col-2"></div>
-  </div>
-
-     <div class="row">
-   <div class="col-12">
+                <div class="row">
+                    <div class="col-12">
                         
-    <table class="table table-hover table-striped">
-   <thead>
-    <tr>
-    <th scope="col">Id</th>
-   <th scope="col">Descrição</th>
-   <th scope="col">Valor</th>
-      <th scope="col">Código de barras</th>
-          <th scope="col"></th>
-   </tr>
-       </thead>
-   <tbody>
-
-
-  <?php 
+                        <table class="table table-hover table-striped">
+                        <thead>
+                            <tr>
+                                <th scope="col">Id</th>
+                                <th scope="col">Descrição</th>
+                                <th scope="col">Valor</th>
+                                <th scope="col">Código de barras</th>
+                                <th scope="col">Imagem</th>
+                                <th scope="col"></th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <?php 
                             
-    if ($resultado->num_rows > 0) {
-    while($row = $resultado->fetch_assoc()) {
-   echo "<tr>";
-   echo "<td>" . $row["Id"] . "</td>";
- echo "<td>" . $row["Descricao"] . "</td>";
-echo "<td>" . $row["Valor"] . "</td>";
-   echo "<td>" . $row["Codigo_barras"] . "</td>";
-  echo "<td><a href='editar_produto.php?id=$row[Id]' class='btn btn-warning' >Editar</a>  ";
-  echo "<a href='excluir_produto.php?id=$row[Id]' class='btn btn-danger'>Excluir</a></td>";
-     echo "</tr>";
-        }
-           } else {
-     echo "<tr><td colspan='3'>Nenhum registro encontrado</td></tr>";
-    }
+                            if ($resultado->num_rows > 0) {
+                                while($row = $resultado->fetch_assoc()) {
+                                    echo "<tr>";
+                                    echo "<td>" . $row["Id"] . "</td>";
+                                    echo "<td>" . $row["Descricao"] . "</td>";
+                                    echo "<td>" . $row["Valor"] . "</td>";
+                                    echo "<td>" . $row["Codigo_barras"] . "</td>";
+                                    echo "<td>" . $row["Imagem"] . "</td>";
+                                    echo "<td><a href='editar_produto.php?Id=$row[Id]' class='btn btn-warning' >Editar</a>  ";
+                                    echo "<a href='excluir_produto.php?Id=$row[Id]' class='btn btn-danger'>Excluir</a></td>";
+                                    echo "</tr>";
+                                }
+                            } else {
+                                echo "<tr><td colspan='3'>Nenhum registro encontrado</td></tr>";
+                            }
                             ?>
                                                     
-  </tbody>
-   </table>
-    </div>
-   </div>
-</div>
-      </div>
+                        </tbody>
+                        </table>
+                    </div>
+                </div>
+
+            </div>
+        </div>
     </div>
 </div>
 
